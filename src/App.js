5d4f236346f4ed0e0486a2a8cd5633c17d9eec16
HAI DO VAN <dvhai@misa-group.vn>
2022-05-05 18:11:36 +0700

    add

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import './App.css';
 function App() {
 
   const responseGoogle = (res) => {
-    var profile = res.getBasicProfile();
-    alert(res.profileObj.givenName)
-    debugger
+    console.log('res', res)
   }
 
   const logout = (res) => {
@@ -16,7 +14,6 @@ function App() {
   }
 
   const renderChildren = (props) => {
-    debugger
     return <button onClick={props.onClick}>login</button>
   }
 
@@ -26,28 +23,72 @@ function App() {
       onSuccess={onSuccess}
       onFailure={onFailure}
       cookiePolicy={'single_host_origin'}
+      responseType="code"
+      accessType='offline'
       render={(innerProps) => renderChildren(innerProps)}
+      scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/adwords"
       {...props}
     />
   }
 
   function Login() {
-    FB.getLoginStatus(function (response) {
-      debugger
-      if (response.status === 'connected') {
-        var uid = response.authResponse.userID;
-        var accessToken = response.authResponse.accessToken;
-      } else if (response.status === 'not_authorized') {
-        FB.login(function (response) {
-          debugger
-        })
-      } else {
-        FB.login(function (response) {
-          debugger
-        })
-      }
-    });
 
+    FB.api('/me/permissions', 'delete', function (response) { });
+    setTimeout(() => {
+      FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+          window.localStorage.setItem("FACE_BOOK_INFO", JSON.stringify((response.authResponse.accessToken)))
+        } else if (response.status === 'not_authorized') {
+          FB.login(function (response) {
+            window.localStorage.setItem("FACE_BOOK_INFO", JSON.stringify((response.authResponse.accessToken)))
+          })
+        } else {
+          FB.login(function (response) {
+            window.localStorage.setItem("FACE_BOOK_INFO", JSON.stringify((response.authResponse.accessToken)))
+          })
+        }
+      });
+    }, 0);
+
+  }
+
+  function Logout() {
+    // FB.api('/me/permissions', 'delete', function (response) {
+    //   alert(JSON.stringify(response))
+    //   Login()
+    // });
+    FB.logout(function (res) { })
+
+  }
+
+  function popitup(url, windowName) {
+    let newwindow = window.open(url, windowName, 'height=600,width=600,toolbar=0,menubar=0,left=50%,top=50%');
+    if (window.focus) { newwindow.focus() }
+    return false;
+  }
+
+  const popupCenter = ({ url, title, w, h }) => {
+    // Fixes dual-screen position                             Most browsers      Firefox
+    const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft
+    const top = (height - h) / 2 / systemZoom + dualScreenTop
+    const newWindow = window.open(url, title,
+      `
+      scrollbars=yes,
+      width=${w / systemZoom}, 
+      height=${h / systemZoom}, 
+      top=${top}, 
+      left=${left}
+      `
+    )
+
+    if (window.focus) newWindow.focus();
   }
 
   return (
@@ -69,15 +110,42 @@ function App() {
         cookiePolicy={'single_host_origin'}
         render={(props) => (<button onClick={props.onClick}>test</button>)}
       /> */}
-      {GGElement(renderChildren, responseGoogle, responseGoogle)}
+      {/* {GGElement(renderChildren, responseGoogle, responseGoogle)} */}
       <GoogleLogout
         clientId="13516959677-pajthcvvv5or0oatfpk1j1kf0n7t8na2.apps.googleusercontent.com"
         buttonText="Logout"
         onLogoutSuccess={logout}
         onFailure={() => alert("logout failed")}
-      ></GoogleLogout>
 
+      ></GoogleLogout>
+      <GoogleLogin
+        clientId="13516959677-pajthcvvv5or0oatfpk1j1kf0n7t8na2.apps.googleusercontent.com"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        buttonText="Sign in with Google"
+        cookiePolicy={'single_host_origin'}
+        responseType="code"
+        accessType='offline'
+        render={(props) => <button onClick={props.onClick}>Login gg</button>}
+        prompt="select_account"
+        scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/adwords"
+      />
       <button onClick={Login}>Login fb</button>
+      <button onClick={Logout}>Logout fb</button>
+      <button onClick={() => {
+        FB.api("/me", res => {
+          if (!res || res.error) {
+            Login()
+          }
+          console.log(res);
+        }, { accessToken: window.localStorage.getItem("FACE_BOOK_INFO") })
+      }}>Get me</button>
+      <button onClick={() => {
+
+        popupCenter({ url: "https://www.facebook.com/", title: "", w: 600, h: 600 })
+
+
+      }}>show popup facebook</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './extensions.js'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
